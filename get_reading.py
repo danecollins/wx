4df301624acbf4584336f67e9bfea35507b57282
Dane Collins <dane@dacxl.com>
2019-08-30 09:01:54 -0700

    Add daily summary

--- get_reading.py
+++ get_reading.py
@@ -4,12 +4,26 @@ import os
 import json
 import datetime
 import pandas as pd
-
-import twilio
-from twilio.rest import TwilioRestClient
-
+import glob
+from postmarker.core import PostmarkClient
 from wu_int import get_station_data, STATION_LIST
 from log import log
+
+# import twilio
+# from twilio.rest import TwilioRestClient
+
+# Postmarker library - https://github.com/Stranger6667/postmarker/
+# docs - https://postmarker.readthedocs.io/en/stable/
+token = os.environ['PM_TOKEN']
+
+
+def pm_send_message(subject: str, body: str, body_type: str='text') -> None:
+    postmark = PostmarkClient(server_token=token)
+    if body_type == 'text':
+        return postmark.emails.send(From='dane@dacxl.com', To='dane@awr.com', Subject=subject, TextBody=body)
+    else:
+        return postmark.emails.send(From='dane@dacxl.com', To='dane@awr.com', Subject=subject, HtmlBody=body)
+
 
 def write_readings_json(readings: List["Reading"], filename: str) -> None:
     with open(filename, 'w') as fp:
@@ -20,6 +34,7 @@ def write_readings_parquet(readings: List["Reading"], filename: str) -> None:
     data = [x.to_dict() for x in readings]
     df = pd.DataFrame.from_records(data)
     df.to_parquet(filename)
+    log(f'Readings logged to {filename}')
 
 
 def readings_to_file(fn: str, format: str='json') -> None:
@@ -34,30 +49,69 @@ def readings_to_file(fn: str, format: str='json') -> None:
         raise(ValueError, f'Illegal format "{format}"')
 
 
-def check_rain():
-    station_ids = [('KCASANJO644', 'Home'),
-                   ('KCASANTA746', 'Beach'),
-                   ('KORPORTL125', 'Portland')]
-    for station, desc in station_ids:
-        incr = Reading.check_for_increase(station)
-        if incr:
-            msg = 'Rain has reached {} inches at station {}'.format(incr, desc)
-            log(msg)
-            sms(msg)
+def read_day(date):
+    pattern = f'./data/{date[:4]}/wx_{date}*.pq'
+    files = glob.glob(pattern)
+    print(files)
+    data = [pd.read_parquet(f) for f in files]
+    df = pd.concat(data)
+    return df
 
 
-def sms(msg):
-    account_sid = os.environ.get('TWILIO_ACCOUNT_SID') or 'ACCOUNT_MISSING'
-    auth_token = os.environ.get('TWILIO_AUTH_TOKEN') or 'AUTH_TOKEN_MISSING'
-    from_num = os.environ.get('TWILIO_FROM')
-    to_num = os.environ.get('TWILIO_TO')
-    client = TwilioRestClient(account_sid, auth_token)
-    try:
-        client.messages.create(to=to_num, from_=from_num, body=msg)
-        log('sms: ' + msg)
-    except twilio.TwilioRestException as e:
-        m = 'Twilio returned error {}'.format(e)
-        log(m, error=True)
+def send_daily_summary(date):
+    df = read_day(date)
+    r = df.sort_values(['temperature', 'feelslike'], ascending=False).iloc[0]
+    high_temp = r.temperature
+    high_loc = r['name']
+
+    r = df.sort_values(['precip_today', 'precip_rate'], ascending=False).iloc[0]
+    if r.precip_today == 0.0:
+        rain_msg = 'There was no rain today.'
+    else:
+        rain_msg = f'There was a total of {r.precip_today}" rain at station {r["name"]}.'
+    
+    r = df.sort_values(['wind_gust_speed'], ascending=False).iloc[0]
+    wind_high = r.wind_gust_speed
+    wind_loc = r['name']
+    msg = f"""
+This is the daily summary for {date}
+
+The highest temperature today was {high_temp} at station {high_loc}.
+{rain_msg}
+The highest wind gust was {wind_high} mph at station {wind_loc}.
+
+
+message sent from get_reading.py
+"""
+    print(msg)
+    pm_send_message('Daily weather summary', msg, body_type='text')
+
+
+
+# def check_rain():
+#     station_ids = [('KCASANJO644', 'Home'),
+#                    ('KCASANTA746', 'Beach'),
+#                    ('KORPORTL125', 'Portland')]
+#     for station, desc in station_ids:
+#         incr = Reading.check_for_increase(station)
+#         if incr:
+#             msg = 'Rain has reached {} inches at station {}'.format(incr, desc)
+#             log(msg)
+#             sms(msg)
+
+
+# def sms(msg):
+#     account_sid = os.environ.get('TWILIO_ACCOUNT_SID') or 'ACCOUNT_MISSING'
+#     auth_token = os.environ.get('TWILIO_AUTH_TOKEN') or 'AUTH_TOKEN_MISSING'
+#     from_num = os.environ.get('TWILIO_FROM')
+#     to_num = os.environ.get('TWILIO_TO')
+#     client = TwilioRestClient(account_sid, auth_token)
+#     try:
+#         client.messages.create(to=to_num, from_=from_num, body=msg)
+#         log('sms: ' + msg)
+#     except twilio.TwilioRestException as e:
+#         m = 'Twilio returned error {}'.format(e)
+#         log(m, error=True)
 
 
 if __name__ == '__main__':
@@ -65,4 +119,8 @@ if __name__ == '__main__':
     #fn = time.strftime('wx_%Y-%m-%d_%H%M%S.json')
     #readings_to_file(fn)
     fn = time.strftime('wx_%Y-%m-%d_%H%M%S.pq')
-    readings_to_file(fn, format='parquet')
+    pathname = f'./data/{time.year}/{fn}'
+    readings_to_file(pathname, format='parquet')
+    if time.hour >= 20:
+        send_daily_summary(time.strftime('%Y-%m-%d'))
+
