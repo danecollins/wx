f090a1bcdadce30bb3bea9c5fed5c549c041d6dc
Dane Collins <dane@awr.com>
2019-09-01 19:40:37 +0000

    Add better logging

--- getdata.sh
+++ getdata.sh
@@ -3,7 +3,9 @@
 source ~/env/wx/bin/activate
 source ~/src/wx/wx_env
 cd /home/dane/src/wx
+echo "----------- git pull to make sure code is up to date --------"
 git pull
+echo "-------------------------------------------------------------"
 python get_reading.py
 curl -L http://w4e.herokuapp.com/checkin/KHQSVXIG
 
