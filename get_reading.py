5917df8f8177625484f981a444bbd6a1ef2bdb70
Dane Collins <dane@dacxl.com>
2021-12-01 18:18:38 -0800

    Fix dict access error

--- get_reading.py
+++ get_reading.py
@@ -58,7 +58,7 @@ def readings_to_file(fn: str, format: str='json') -> None:
                 readings.append(data)
                 station_ids.remove(station)
                 # check rain for a limited set of stations
-                if STATION_LIST[station].rain:
+                if STATION_LIST[station]['rain']:
                     check_rain(station, data.precip_today)
             sleep(2)
         retry_count -= 1
